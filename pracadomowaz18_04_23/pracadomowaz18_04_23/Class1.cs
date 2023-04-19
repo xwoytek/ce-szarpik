e5077b1772f1e1cea535cb772d83cad80ca1fd53
xwoytek <115814508+xwoytek@users.noreply.github.com>
2023-04-19 16:29:15 +0200

    Add files via upload

---
+++ pracadomowaz18_04_23/pracadomowaz18_04_23/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracadomowaz18_04_23
+{
+    internal class wzory
+    {
+        public static double pkwadratu(int a)
+        {
+            return Math.Pow(a, 2);
+        }
+        public static double pprostokata(int a,int b)
+        {
+            return a * b;
+        }
+        public static double ptrjkt(int a, int h)
+        {
+            return a * h/2;
+        }
+        public static double prowno(int a, int h)
+        {
+            return a * h;
+        }
+        public static double ptrapez(int a,int b, int h)
+        {
+            return (a+b)*h/2;
+        }
+        public static double pkola(int r)
+        {
+            return Math.PI *Math.Pow(r,2);
+        }
+        public static double objszesc(int a)
+        {
+            return Math.Pow(a, 3);
+        }
+        public static double objprostopadloscianu(int a,int b, int c)
+        {
+            return a*b*c;
+        }
+        public static double objwalca(int r,int h)
+        {
+            return Math.PI * Math.Pow(r, 2)* h;
+        }
+        public static double objkuli(int r)
+        {
+            return Math.PI * Math.Pow(r, 2)*4/3;
+        }
+        public static double pszesc(int a)
+        {
+            return a*6;
+        }
+        public static double pprostopadl(int a,int b,int c)
+        {
+            return 2*a*b+2*b*c+2*a*c;
+        }
+        public static double pwalca(int r, int h)
+        {
+            return Math.PI * Math.Pow(r, 2)+Math.PI*2*r*h;
+        }
+    }
+}
---
+++ pracadomowaz18_04_23/pracadomowaz18_04_23/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pracadomowaz18_04_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0 - pole kwadratu, 1 - pole prostokata, 2 - pole trojkata, 3 - pole rownolegloboku, 4 - pole trapezu, 5 - pole kola, 6 - objetosc szescianu,");
+            Console.WriteLine("7 - objetosc prostopadloscianu, 8 - objetosc walca, 9 - objetosc kuli, 10 - pole powierzchni szescianu, 11 - pole powierzchni prostopadloscianu, 12 - pole powierzchni walca");
+            int inp = int.Parse(Console.ReadLine());
+            switch (inp)
+            {
+                case 0:
+                    Console.WriteLine("dej a");
+                    Console.WriteLine(wzory.pkwadratu(int.Parse(Console.ReadLine())));
+                    break;
+                case 1:
+                    Console.WriteLine("dej a,b");
+                    Console.WriteLine(wzory.pprostokata(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine())));
+                    break;
+                case 2:
+                    Console.WriteLine("dej a,h");
+                    Console.WriteLine(wzory.ptrjkt(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 3:
+                    Console.WriteLine("dej a,h");
+                    Console.WriteLine(wzory.prowno(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 4:
+                    Console.WriteLine("dej a,b,c");
+                    Console.WriteLine(wzory.ptrapez(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 5:
+                    Console.WriteLine("dej r");
+                    Console.WriteLine(wzory.pkola(int.Parse(Console.ReadLine())));
+                    break;
+                case 6:
+                    Console.WriteLine("dej a");
+                    Console.WriteLine(wzory.objszesc(int.Parse(Console.ReadLine())));
+                    break;
+                case 7:
+                    Console.WriteLine("dej a,b,c");
+                    Console.WriteLine(wzory.objprostopadloscianu(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 8:
+                    Console.WriteLine("dej r,h");
+                    Console.WriteLine(wzory.objwalca(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 9:
+                    Console.WriteLine("dej r");
+                    Console.WriteLine(wzory.objkuli(int.Parse(Console.ReadLine())));
+                    break;
+                case 10:
+                    Console.WriteLine("dej a");
+                    Console.WriteLine(wzory.pszesc(int.Parse(Console.ReadLine())));
+                    break;
+                case 11:
+                    Console.WriteLine("dej a,b,c");
+                    Console.WriteLine(wzory.pprostopadl(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                case 12:
+                    Console.WriteLine("dej r,h");
+                    Console.WriteLine(wzory.pwalca(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                    break;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}

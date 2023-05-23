9f55a78df8a222645ea8087712f2e85048b6d0c9
xwoytek <115814508+xwoytek@users.noreply.github.com>
2023-05-23 18:09:09 +0200

    Add files via upload

---
+++ projektkoncowynainfetenfaktycznyczyliuno/projektkoncowynainfetenfaktycznyczyliuno/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace projektkoncowynainfetenfaktycznyczyliuno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dialogi.poczotek();
+            int ilekart = karty.ilekart();
+            bool win1 = false;
+            bool win2 = false;
+            
+            string[] cardsplayer1 = karty.shuffle(ilekart);
+            
+            string[] cardsplayer2 = karty.shuffle2(ilekart);
+            Random rnd1 = new Random();
+            string lastcard = karty.randomcard(rnd1);
+            label2137:;
+            Random rnd2 = new Random();
+            if (lastcard.Contains("+4") || lastcard.Contains("chcol"))
+            {
+                lastcard = karty.randomcard(rnd2);
+                goto label2137;
+            }
+            int n1 = 0;
+            int n2 = 0;
+            
+            while (!win1 || !win2)
+            {
+                //start gracz 1
+                label1:;
+
+                foreach (string card in cardsplayer1)
+                {
+                    if (card == null)
+                    {
+                        n1++;
+                    }
+                }
+                foreach(string card in cardsplayer2)
+                {
+                    if (card == null)
+                    {
+                        n2++;
+                    }
+                }
+                if (n1 >= 100 || n2 >= 100)
+                {
+                    if (n1>n2)
+                    {
+                        win1 = true;
+                        n1 = 0; n2 = 0 ;
+                        break;
+                    }
+                    if (n2>n1)
+                    {
+                        win2 = true;
+                        n1 = 0; n2 = 0;
+                        break;
+                    } 
+                }
+                n1 = 0;
+                n2 = 0;
+                string odp1 = dialogi.gracz1(cardsplayer1,lastcard);
+                Random rnd3 = new Random();
+                for (int i = 0;i < cardsplayer1.Length; i++)
+                {
+                    if (cardsplayer1[i] == odp1 && karty.checkcard(lastcard,cardsplayer1[i]))
+                    {
+                        lastcard = cardsplayer1[i];
+                        cardsplayer1[i] = null;
+                        if (odp1 == "chcol" || odp1 == "+4")
+                        {
+                            Console.WriteLine("Jaki kolor wybierasz");
+                            string odpkol1 = Console.ReadLine().ToLower();
+                            lastcard += odpkol1;
+                        }
+                        string cz1 = karty.specjal(odp1);
+                        if (cz1 == "skip"|| cz1 == "reverse")
+                        {
+                            goto label1;
+                        }
+                        if (cz1 == "+2")
+                        {
+                            for (int  j = 0;j < 2; j++)
+                            {
+                                for (int k = 0;k < cardsplayer2.Length; k++)
+                                {
+                                    if (cardsplayer2[k] == null)
+                                    {
+                                        cardsplayer2[k] = karty.randomcard(rnd3);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        Random rnd4 = new Random();
+                        if (cz1 == "+4")
+                        {
+                            for (int j = 0; j < 4; j++)
+                            {
+                                for (int k = 0; k < cardsplayer2.Length; k++)
+                                {
+                                    if (cardsplayer2[k] == null)
+                                    {
+                                        cardsplayer2[k] = karty.randomcard(rnd4);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Random rnd5 = new Random();
+                if (odp1 == "ciagne")
+                {
+                    for (int i = 0; i<cardsplayer1.Length ; i++)
+                    {
+                        string loskart = karty.randomcard(rnd5);
+                        
+                        if (cardsplayer1[i] == null)
+                        {
+                            cardsplayer1[i] = loskart;
+                            break;  
+                        }
+                    }
+                }
+                //koniec gracz 1
+                while (!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!false)
+                {
+                    string Hello = "Hello,";
+                    string space = " ";
+                    string World = "World!";
+                    Console.WriteLine(Hello,space,World);
+                }
+                //start gracz 2
+                label2:;
+                foreach (string card in cardsplayer1)
+                {
+                    if (card == null)
+                    {
+                        n1++;
+                    }
+                }
+                foreach (string card in cardsplayer2)
+                {
+                    if (card == null)
+                    {
+                        n2++;
+                    }
+                }
+                if (n1 >= 100 || n2 >= 100)
+                {
+                    if (n1 > n2)
+                    {
+                        win1 = true;
+                        n1 = 0; n2 = 0;
+                        break;
+                    }
+                    if (n2 > n1)
+                    {
+                        win2 = true;
+                        n1 = 0; n2 = 0;
+                        break;
+                    }
+                }
+                n1 = 0; 
+                n2 = 0;
+                string odp2 = dialogi.gracz2(cardsplayer2, lastcard);
+
+                for (int i = 0; i < cardsplayer2.Length; i++)
+                {
+                    if (cardsplayer2[i] == odp2 && karty.checkcard(lastcard, cardsplayer2[i]))
+                    {
+                        lastcard = cardsplayer2[i];
+                        cardsplayer2[i] = null;
+                        if (odp2 == "chcol" || odp2 == "+4")
+                        {
+                            Console.WriteLine("Jaki kolor wybierasz");
+                            string odpkol2 = Console.ReadLine().ToLower();
+                            lastcard += odpkol2;  
+                        }
+                        string cz2 = karty.specjal(odp2);
+                        Random rnd6 = new Random();
+                        if (cz2 == "skip" || cz2 == "reverse")
+                        {
+                            goto label2;
+                        }
+                        if (cz2 == "+2")
+                        {
+                            for (int j = 0; j < 2; j++)
+                            {
+                                for (int k = 0; k < cardsplayer1.Length; k++)
+                                {
+                                    if (cardsplayer1[k] == null)
+                                    {
+                                        cardsplayer1[k] = karty.randomcard(rnd6);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        Random rnd7 = new Random();
+                        if (cz2 == "+4")
+                        {
+                            for (int j = 0; j < 4; j++)
+                            {
+                                for (int k = 0; k < cardsplayer1.Length; k++)
+                                {
+                                    if (cardsplayer1[k] == null)
+                                    {
+                                        cardsplayer1[k] = karty.randomcard(rnd7);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Random rnd8 = new Random();
+                if (odp2 == "ciagne")
+                {
+                    for (int i = 0; i < cardsplayer2.Length; i++)
+                    {
+                        string loskart = karty.randomcard(rnd8);
+
+                        if (cardsplayer2[i] == null)
+                        {
+                            cardsplayer2[i] = loskart;
+                            break;
+                        }
+                    }
+                }
+                //koniec gracz 2
+            }
+            if (n1 >= 100 || n2 >= 100)
+            {
+                if (n1 > n2)
+                {
+                    win1 = true;
+                    
+                }
+                if (n2 > n1)
+                {
+                    win2 = true;
+                    
+                }
+            }
+            if (win1)
+            {
+                Console.WriteLine("Brawo Gracz1 wygrał");
+            }
+            if (win2)
+            {
+                Console.WriteLine("Brawo Gracz2 wygrał");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ projektkoncowynainfetenfaktycznyczyliuno/projektkoncowynainfetenfaktycznyczyliuno/dialogi.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace projektkoncowynainfetenfaktycznyczyliuno
+{
+    internal class dialogi
+    {
+        public static void poczotek()
+        {
+            Console.WriteLine("Witaj w grze UNO!");
+            Console.WriteLine("Jest to gra dwuosobowa");
+            
+        }
+        public static string gracz1(string[] a,string b)
+        {
+            
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            Console.WriteLine("Teraz Gracz 1");
+            Console.ReadLine();
+            
+            foreach (string card in a)
+            {
+                karta(card);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("To są twoje karty");
+            lastcard(b);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\njaką karte dajesz czy ciągnies?");
+            string odp = Console.ReadLine().ToLower();
+            return odp;
+            
+            
+        } 
+        public static string gracz2(string[] a,string b)
+        {
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            Console.WriteLine("Teraz Gracz 2");
+            Console.ReadLine();
+            foreach (string card in a)
+            {
+                karta(card);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("To są twoje karty");
+            lastcard(b);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\njaką karte dajesz czy ciągniesz?");
+            string odp = Console.ReadLine().ToLower();
+            return odp;
+        }
+        public static void karta(string b)
+        {
+            string a = "";
+            if (b == null)
+            {
+                goto Label;
+            }
+            if (b.Contains("cz"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            if (b.Contains("b"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            if (b.Contains("y"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            if (b.Contains("g"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (b.Contains("0"))
+            {
+                a = "0";
+            }
+            if (b.Contains("1"))
+            {
+                a = "1";
+            }
+            if (b.Contains("2"))
+            {
+                a = "2";
+            }
+            if (b.Contains("3"))
+            {
+                a = "3";
+            }
+            if (b.Contains("4"))
+            {
+                a = "4";
+            }
+            if (b.Contains("5"))
+            {
+                a = "5";
+            }
+            if (b.Contains("6"))
+            {
+                a = "6";
+            }
+            if (b.Contains("7"))
+            {
+                a = "7";
+            }
+            if (b.Contains("8"))
+            {
+                a = "8";
+            }
+            if (b.Contains("9"))
+            {
+                a = "9";
+            }
+            if (b.Contains("skip"))
+            {
+                a = "o";
+            }
+            if (b.Contains("reverse"))
+            {
+                a = "$";
+            }
+            if (b.Contains("+2"))
+            {
+                a = "Z";
+            }
+            if (b.Contains("+4"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                a = "A";
+                if (b.Contains("cz"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (b.Contains("b"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                if (b.Contains("y"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                if (b.Contains("g"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            }
+            if (b.Contains("chcol"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                a = "C";
+                if (b.Contains("cz"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (b.Contains("b"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                if (b.Contains("y"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                if (b.Contains("g"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"{a}--------{a}", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"|   {a}    |", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"|        |", Console.ForegroundColor);
+            Console.WriteLine($"{a}--------{a}", Console.ForegroundColor);
+            Console.WriteLine($"^Ta karta to {b}^");
+            Label:;
+        }
+        public static void lastcard(string a)
+        {
+            Console.WriteLine("Ostatnia karta to  VVV");
+            karta(a);
+            Console.WriteLine("Ostatnia karta to  ^^^");
+        }
+
+
+    }
+}
